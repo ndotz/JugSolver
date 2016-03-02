517d01b85a987c05ddf02c35dc6fe30a93c24afc
ndotz <ndotz@dcac5bd5-10e9-ac4f-83d8-a8de0d7f6ee2>
2016-03-02 07:19:47 +0000

    Removed some comments and printing to the console.

--- jug.hs
+++ jug.hs
@@ -1,23 +1,18 @@
 module Main where
-	--import Prelude hiding (null, lookup, map, filter)
-	--import Data.HashMap.Lazy as HashMap		
 	import Data.Map
-	import qualified Data.Set as Set
-	--import Data.Set
+	import qualified Data.Set as Set	
 	import Data.Maybe
 	import Data.Graph.AStar	
 	import Debug.Trace
 	import Data.List
 
-	--goal = 4
-	--jugsList = [3,5]
-	goal = 35
-	jugsList = [15, 28, 42, 44]
+	goal = 4
+	jugsList = [3,5]
+	--goal = 35
+	--jugsList = [15, 28, 42, 44]
 	startVertex = (1,1) : (Prelude.map (\x -> (x, 0)) jugsList)
-	goalFunc jugs = (snd (last jugs)) == goal
-	--distanceToGoal jugs = abs ((snd (last jugs)) - goal)
+	goalFunc jugs = (snd (last jugs)) == goal	
 	distanceToGoal jugs = minimum ( Prelude.map (\jug -> abs ( snd jug ) - goal) jugs)
-	--distanceToGoal jugs = 0
 	distanceToNeighbor x y = 1
 	main = do		
 		print $ aStar graph distanceToNeighbor distanceToGoal goalFunc startVertex
@@ -41,16 +36,7 @@ module Main where
 		where
 			jugHash = Data.Map.fromList jugs
 
-	--graph jugs = trace ("\n vertex ya on: " ++ (show jugs) ++ "\n neighbors " ++ (show neighbors) ++ "\n") neighbors
-	--	where
-	--		pourings = [ ( (pour (fst jugTo) (fst jugFrom) jugs) ) ++ [( (fromJust (elemIndex jugFrom jugsList) ), (fromJust (elemIndex jugTo jugsList)) )] 
-	--			| jugTo <- jugs, jugFrom <- jugs,
-	--			jugFrom /= jugTo, (snd jugFrom) > 0, (snd jugTo) /= (fst jugTo)]
-	--		fillings = [ (fill (fst jugTo) jugs) ++ [( -1 , (fromJust (elemIndex jugTo jugsList)) )] | jugTo <- jugs, (fst jugTo) /= (snd jugTo)]
-	--		emptyings = [ (emptyDatJug (fst jugTo) jugs) | jugTo <- jugs, (snd jugTo) > 0]
-	--		neighbors = Set.fromList (pourings ++ fillings ++ emptyings)
-
-	graph vertex = trace ("\n vertex ya on: " ++ (show jugs) ++ "\n neighbors " ++ (show neighbors) ++ "\n") neighbors
+	graph vertex = neighbors
 		where
 			pourings = [ [( (fromJust (elemIndex (fst jugFrom) jugsList) ), (fromJust (elemIndex (fst jugTo) jugsList)) )] ++ ( (pour (fst jugTo) (fst jugFrom) jugs) )
 				| jugTo <- jugs,
@@ -67,11 +53,3 @@ module Main where
 				(snd jugTo) > 0]
 			neighbors = Set.fromList (pourings ++ fillings ++ emptyings)
 			jugs = tail vertex
-
-	--graphTest = ( graph [(15,2), (28, 1), (42, 15), (44, 44)] )
-	graphTest = ( graph startVertex )
-	pourTest = pour 15 28 [(15,2), (28, 1), (42, 15), (44, 44)]
-
-	--main = putStrLn "Hello, World!"
-	--main = do
-	--	print $ aStar graph distanceNeighbors distance goal 1 	
